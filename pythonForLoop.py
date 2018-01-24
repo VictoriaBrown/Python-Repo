21f65065e9f0791fa9e0003b86e61535399f628b
Victoria Brown <victoriabrown0710@gmail.com>
2018-01-24 08:27:20 -0600

    For Loops

---
+++ pythonForLoop.py
@@ -0,0 +1,8 @@
+for letter in 'Python':                 # First example
+    print "Current Letter: ", letter
+
+fruits = ['banana', 'apple', 'mango']
+for fruit in fruits:                    # Second example
+    print 'Current Fruit: ', fruit
+
+print "Good bye!"
---
+++ pythonForLoop2.py
@@ -0,0 +1,5 @@
+fruits = ['banana', 'apple', 'mango']
+for index in range(len(fruits)):
+    print 'Current Fruit: ', fruits[index]
+
+print 'Good bye!'
